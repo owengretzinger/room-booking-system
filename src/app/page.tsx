a21128d374ce9849770bb0e2c45d9c7e72218998
geon-youn <geon.youn@outlook.com>
2023-12-14 16:04:42 -0500

    move header from layout to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
-import Image from 'next/image'
+import Header from './Header';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
-    </main>
-  )
+    <>
+      <Header back={undefined} next={undefined} state_progress={undefined}></Header>
+      <main className="flex min-h-screen flex-col items-center justify-between"></main>
+    </>
+  );
 }
