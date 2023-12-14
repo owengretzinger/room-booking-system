3595248baabc40461eb5df4d7453012140813249
geon-youn <geon.youn@outlook.com>
2023-12-14 16:18:33 -0500

    add source of login request to login for cancel or confirming the booking

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,9 +1,17 @@
 import Header from '../Header';
 
-export default function Login() {
+interface Login {
+  origin: string;
+}
+
+export default function Login({ origin }: Login) {
   return (
     <>
-      <Header back={'rooms'} next={'confirm'} state_progress={3}></Header>
+      {origin === 'rooms' ? (
+        <Header back={'rooms'} next={'confirm'} state_progress={3}></Header>
+      ) : origin === '/' ? (
+        <Header back={'/'} next={'cancel'} state_progress={3}></Header>
+      ) : null}
       <main className="flex min-h-screen flex-col items-center justify-between"></main>
     </>
   );
