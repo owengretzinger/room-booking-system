ef7d66b804d65f44dce7b321bbf666418ca47bb2
geon-youn <geon.youn@outlook.com>
2023-12-15 17:15:35 -0500

    add states for header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import Header from '../components/Header';
 import CancelPage from './pages/CancelPage';
 import ConfirmPage from './pages/ConfirmPage';
@@ -7,14 +9,18 @@ import LoginPage from './pages/LoginPage';
 import MainPage from './pages/MainPage';
 import RoomsPage from './pages/RoomsPage';
 
+import { useState } from 'react';
+
 export default function Home() {
+  const [back, setBack] = useState(undefined);
+  const [next, setNext] = useState(undefined);
+  const [stateProgress, setStateProgress] = useState(undefined);
+
+  const headerProps = { back, next, state_progress: stateProgress };
+
   return (
     <>
-      <Header
-        back={undefined}
-        next={undefined}
-        state_progress={undefined}
-      ></Header>
+      <Header {...headerProps}></Header>
     </>
   );
 }
