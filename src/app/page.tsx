bdaf71bc4f422cab6a8a307bef781213600cff20
geon-youn <geon.youn@outlook.com>
2023-12-15 17:12:45 -0500

    import pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,11 @@
 import Header from '../components/Header';
+import CancelPage from './pages/CancelPage';
+import ConfirmPage from './pages/ConfirmPage';
+import DatetimePage from './pages/DatetimePage';
+import FiltersPage from './pages/FiltersPage';
+import LoginPage from './pages/LoginPage';
+import MainPage from './pages/MainPage';
+import RoomsPage from './pages/RoomsPage';
 
 export default function Home() {
   return (
