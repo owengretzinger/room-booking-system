5549f4c8dcf31db8009d2e00cb1cf0f6898f5fd5
kennethsalim <97636487+kennethsalim@users.noreply.github.com>
2023-12-15 18:35:05 -0500

    Added confirmation page (#10)
    Co-authored-by: kennethkvs <kennethkvs060103@gmail.com>

---
+++ src/components/RoomDetails.tsx
@@ -0,0 +1,50 @@
+"use client";
+import React from "react";
+
+interface RoomDetails {
+  isLoggedIn: boolean;
+}
+
+export default function RoomDetails({ isLoggedIn }: RoomDetails) {
+  return (
+    <div className="flex flex-col items-start">
+      {/* Title */}
+      <h1 className="text-2xl font-bold text-red-925 mb-2">Room Details</h1>
+      {/* Boooking Info */}
+      <div className="flex flex-col items-start border-4 border-red-925 px-10 py-5 rounded-lg">
+        <h2 className="text-xl font-bold my-2">Gerald Hatch Centre H204A</h2>
+        <p className="text-lg">Date: December 14, 2023</p>
+        <p className="text-lg">Time: 12:30am - 3:30pm</p>
+        <div className="flex flex-row mt-5">
+          <img src="person.svg" />
+          <p className="text-lg text-red-925 algin-middle leading-[3rem]">
+            5-10 people
+          </p>
+        </div>
+        <div className="flex flex-row">
+          <img src="power.svg" />
+          <p className="text-lg text-red-925 algin-middle leading-[3rem]">
+            4 outlets
+          </p>
+        </div>
+        <div className="flex flex-row">
+          <img src="camera.svg" />
+          <p className="text-lg text-red-925 algin-middle leading-[3rem]">
+            Projector
+          </p>
+        </div>
+      </div>
+      {/* Confirmation Button */}
+      <a href="/cancel" className="w-full">
+        <button
+          disabled={!isLoggedIn}
+          className={`${
+            isLoggedIn ? "bg-amber-350" : "bg-slate-500"
+          } text-white rounded-lg w-full py-2 my-5`}
+        >
+          Confirm
+        </button>
+      </a>
+    </div>
+  );
+}
--- src/pages/ConfirmPage.tsx
+++ src/pages/ConfirmPage.tsx
@@ -1,11 +1,98 @@
+
+"use client";
+import React from "react";
+
+import RoomDetails from "@/components/RoomDetails";
+
+let counter = 0;
+     
 interface ConfirmPage {
   setCurrentPage: Function;
 }
 
 export default function ConfirmPage({ setCurrentPage }: ConfirmPage) {
+  const [isLoggedIn, setIsLoggedIn] = React.useState(true);
+  const [email, setEmail] = React.useState<string[]>([]);
+
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement>,
+    index: number
+  ) => {
+    const values = [...email];
+    values[index] = e.target.value;
+    setEmail(values);
+  };
+
+  const handleDeleteField = (
+    e: React.MouseEvent<HTMLButtonElement>,
+    index: number
+  ) => {
+    const values = [...email];
+    values.splice(index, 1);
+    setEmail(values);
+    counter--;
+  };
+
   return (
     <>
-      <main className="flex min-h-screen flex-col items-center justify-between"></main>
+      <main className="flex flex-row items-center justify-center mx-20">
+        <div className="flex flex-col items-start w-1/2 mr-5">
+          {isLoggedIn ? (
+            <>
+              <h1 className="text-2xl font-bold text-red-925">
+                Email Confirmation will be sent to:
+              </h1>
+              <h1 className="text-2xl font-bold text-slate-500">
+                doej@mcmaster.ca
+              </h1>
+              <h1 className="text-2xl font-bold text-red-925 mt-10">
+                Optionally add group member emails to send them confirmation &
+                directions
+              </h1>
+              <div className="flex flex-col items-start w-full">
+                {email.map((email, index) => (
+                  <div key={index} className="flex flex-row flex-nowrap">
+                    <input
+                      className="text-red-925 rounded-md outline-3 outline outline-red-925 w-72 my-2 py-1 px-2 focus:outline-amber-350"
+                      type="email"
+                      placeholder="MacID@mcmaster.ca"
+                      value={email}
+                      onChange={(e) => handleChange(e, index)}
+                    />
+                    <button
+                      className="text-red-925 rounded-md outline-3 outline my-2 mx-3 py-1 px-2"
+                      onClick={(e) => handleDeleteField(e, index)}
+                    >
+                      X
+                    </button>
+                  </div>
+                ))}
+                {counter < 5 ? (
+                  <button
+                    className="text-red-925 rounded-md outline-3 outline-dashed w-[80%] my-2 py-1 px-2"
+                    onClick={() => {
+                      setEmail([...email, ""]);
+                      counter++;
+                    }}
+                  >
+                    Add Email
+                  </button>
+                ) : (
+                  <></>
+                )}
+              </div>
+            </>
+          ) : (
+            <button
+              className="bg-red-925 text-white rounded-lg w-[80%] px-10 py-4 my-5"
+              onClick={() => setIsLoggedIn(true)}
+            >
+              Log In With Avenue
+            </button>
+          )}
+        </div>
+        <RoomDetails isLoggedIn={isLoggedIn} />
+      </main>
     </>
   );
 }
