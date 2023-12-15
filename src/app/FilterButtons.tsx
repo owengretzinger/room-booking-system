f41f06d67034753d11986f0c871a4eb5f0b77a4d
owengretzinger <owengretzinger@gmail.com>
2023-12-15 16:18:23 -0500

    Remove commented code

--- src/app/FilterButtons.tsx
+++ src/app/FilterButtons.tsx
@@ -54,28 +54,3 @@ export default function FilterButtons({ items }: { items: { key: number, name: s
     </>
   )
 }
-
-// interface FilterButtons {
-//   name: string | undefined;
-// }
-
-// export default function FilterButtons({ name }: FilterButtons) {
-//   const [isSelected, setIsSelected] = useState(false);
-
-//   const handleClick = () => {
-//     setIsSelected(!isSelected);
-//   };
-
-//   return (
-// <button
-//   onClick={handleClick}
-//   className={`${
-//     isSelected || name == "Any"
-//       ? "text-white bg-red"
-//       : "text-red outline outline-3"
-//   }  rounded-md px-4 py-2 m-2`}
-// >
-//   <span>{name}</span>
-// </button>
-//   );
-// }
