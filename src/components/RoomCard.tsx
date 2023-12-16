a46799bf0083655b680e28cf119956c8cc10eac6
kennethkvs <kennethkvs060103@gmail.com>
2023-12-15 19:01:46 -0500

    Fix Paths

--- src/components/RoomCard.tsx
+++ src/components/RoomCard.tsx
@@ -2,43 +2,41 @@ import { PiPlugBold, PiTelevision } from "react-icons/pi";
 import { GrGroup } from "react-icons/gr";
 import Image from "next/image";
 
+interface RoomCard {
+  setCurrentPage: Function;
+}
 
-export default function RoomCard() {
-
+export default function RoomCard({ setCurrentPage }: RoomCard) {
   return (
     <div className="flex h-[250px] p-4 border-red border-4 rounded-xl gap-4 w-full md:w-[750px]">
       {/* image */}
       <div className="aspect-square bg-gray-400 rounded-xl hidden sm:block">
-        <Image src="/images/big-bingo.png" width={100} height={100} className="rounded-xl w-full h-full object-cover" alt="Picture of the room" />
-
+        <Image
+          src="/images/big-bingo.png"
+          width={100}
+          height={100}
+          className="rounded-xl w-full h-full object-cover"
+          alt="Picture of the room"
+        />
       </div>
 
       {/* info */}
       <div className="flex flex-col gap-4 text-red ">
-        <div className="text-5xl">
-          BSB 124
-        </div>
+        <div className="text-5xl">BSB 124</div>
         <ul className="flex flex-col gap-2">
           <li className="flex items-center gap-2">
             <GrGroup className="" size={24} />
-            <span className="">
-              8 people
-            </span>
+            <span className="">8 people</span>
           </li>
           <li className="flex items-center gap-2">
             <PiPlugBold className="" size={24} />
-            <span className="">
-              4 outlets
-            </span>
+            <span className="">4 outlets</span>
           </li>
           <li className="flex items-center gap-2">
             <PiTelevision className="" size={24} />
-            <span className="">
-              TV
-            </span>
+            <span className="">TV</span>
           </li>
         </ul>
-
       </div>
 
       {/* match */}
@@ -46,19 +44,18 @@ export default function RoomCard() {
         <div className="h-full flex justify-center items-center">
           <ProgressCircle percentFilled={70} />
         </div>
-        <a href="#" className="w-full text-white bg-amber-350 rounded-xl px-4 py-2 text-center">
+        <button
+          className="w-full text-white bg-amber-350 rounded-xl px-4 py-2 text-center"
+          onClick={() => setCurrentPage("confirm")}
+        >
           Book Now
-        </a>
+        </button>
       </div>
     </div>
-  )
-
+  );
 }
 
-
-
 function ProgressCircle({ percentFilled }: { percentFilled: number }) {
-
   return (
     <div className="relative w-28 h-28 text-red">
       <svg className="w-full h-full" viewBox="0 0 100 100">
@@ -66,7 +63,6 @@ function ProgressCircle({ percentFilled }: { percentFilled: number }) {
         <circle
           className="text-gray-200 stroke-current"
           stroke-width="10"
-
           cx="50"
           cy="50"
           r="40"
@@ -84,11 +80,11 @@ function ProgressCircle({ percentFilled }: { percentFilled: number }) {
           // https://stackoverflow.com/a/77095851
           stroke-dashoffset={`calc(400 - (250 * ${percentFilled}) / 100)`}
         ></circle>
-
       </svg>
       <div className="absolute inset-0 flex items-center justify-center text-center leading-[14px]">
-        {percentFilled}%<br />Match
+        {percentFilled}%<br />
+        Match
       </div>
     </div>
-  )
+  );
 }
--- src/components/RoomDetails.tsx
+++ src/components/RoomDetails.tsx
@@ -3,48 +3,53 @@ import React from "react";
 
 interface RoomDetails {
   isLoggedIn: boolean;
+  setCurrentPage: Function;
 }
 
-export default function RoomDetails({ isLoggedIn }: RoomDetails) {
+export default function RoomDetails({
+  isLoggedIn,
+  setCurrentPage,
+}: RoomDetails) {
   return (
     <div className="flex flex-col items-start">
       {/* Title */}
-      <h1 className="text-2xl font-bold text-red-925 mb-2">Room Details</h1>
+      <h1 className="text-2xl font-bold text-red mb-2">Room Details</h1>
       {/* Boooking Info */}
-      <div className="flex flex-col items-start border-4 border-red-925 px-10 py-5 rounded-lg">
+      <div className="flex flex-col items-start border-4 border-red px-10 py-5 rounded-lg">
         <h2 className="text-xl font-bold my-2">Gerald Hatch Centre H204A</h2>
         <p className="text-lg">Date: December 14, 2023</p>
         <p className="text-lg">Time: 12:30am - 3:30pm</p>
         <div className="flex flex-row mt-5">
           <img src="person.svg" />
-          <p className="text-lg text-red-925 algin-middle leading-[3rem]">
+          <p className="text-lg text-red algin-middle leading-[3rem]">
             5-10 people
           </p>
         </div>
         <div className="flex flex-row">
           <img src="power.svg" />
-          <p className="text-lg text-red-925 algin-middle leading-[3rem]">
+          <p className="text-lg text-red algin-middle leading-[3rem]">
             4 outlets
           </p>
         </div>
         <div className="flex flex-row">
           <img src="camera.svg" />
-          <p className="text-lg text-red-925 algin-middle leading-[3rem]">
+          <p className="text-lg text-red algin-middle leading-[3rem]">
             Projector
           </p>
         </div>
       </div>
       {/* Confirmation Button */}
-      <a href="/cancel" className="w-full">
-        <button
-          disabled={!isLoggedIn}
-          className={`${
-            isLoggedIn ? "bg-amber-350" : "bg-slate-500"
-          } text-white rounded-lg w-full py-2 my-5`}
-        >
-          Confirm
-        </button>
-      </a>
+      <button
+        disabled={!isLoggedIn}
+        className={`${
+          isLoggedIn ? "bg-amber-350" : "bg-slate-500"
+        } text-white rounded-lg w-full py-2 my-5`}
+        onClick={() => {
+          setCurrentPage("done");
+        }}
+      >
+        Confirm
+      </button>
     </div>
   );
 }
