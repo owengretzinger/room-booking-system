22dbed19b0592a888490d8989d7e2c641a4a6b6b
kennethkvs <kennethkvs060103@gmail.com>
2023-12-16 22:31:22 -0500

    Minor style changes

--- src/components/Timepicker.tsx
+++ src/components/Timepicker.tsx
@@ -3,49 +3,56 @@
 import { useEffect, useState } from "react";
 
 const timeslots = [
-  { key: 1, name: '8:00 AM - 8:30 AM' },
-  { key: 2, name: '8:30 AM - 9:00 AM' },
-  { key: 3, name: '9:00 AM - 9:30 AM' },
-  { key: 4, name: '9:30 AM - 10:00 AM' },
-  { key: 5, name: '10:00 AM - 10:30 AM' },
-  { key: 6, name: '10:30 AM - 11:00 AM' },
-  { key: 7, name: '11:00 AM - 11:30 AM' },
-  { key: 8, name: '11:30 AM - 12:00 PM' },
-  { key: 9, name: '12:00 PM - 12:30 PM' },
-  { key: 10, name: '12:30 PM - 1:00 PM' },
-  { key: 11, name: '1:00 PM - 1:30 PM' },
-  { key: 12, name: '1:30 PM - 2:00 PM' },
-  { key: 13, name: '2:00 PM - 2:30 PM' },
-  { key: 14, name: '2:30 PM - 3:00 PM' },
-  { key: 15, name: '3:00 PM - 3:30 PM' },
-  { key: 16, name: '3:30 PM - 4:00 PM' },
-  { key: 17, name: '4:00 PM - 4:30 PM' },
-  { key: 18, name: '4:30 PM - 5:00 PM' },
-  { key: 19, name: '5:00 PM - 5:30 PM' },
-  { key: 20, name: '5:30 PM - 6:00 PM' },
-  { key: 21, name: '6:00 PM - 6:30 PM' },
-  { key: 22, name: '6:30 PM - 7:00 PM' },
-  { key: 23, name: '7:00 PM - 7:30 PM' },
-  { key: 24, name: '7:30 PM - 8:00 PM' },
-  { key: 25, name: '8:00 PM - 8:30 PM' },
-  { key: 26, name: '8:30 PM - 9:00 PM' },
-  { key: 27, name: '9:00 PM - 9:30 PM' },
-  { key: 28, name: '9:30 PM - 10:00 PM' },
-  { key: 29, name: '10:00 PM - 10:30 PM' },
-  { key: 30, name: '10:30 PM - 11:00 PM' },
-  { key: 31, name: '11:00 PM - 11:30 PM' },
-  { key: 32, name: '11:30 PM - 12:00 AM' },
+  { key: 1, name: "8:00 AM - 8:30 AM" },
+  { key: 2, name: "8:30 AM - 9:00 AM" },
+  { key: 3, name: "9:00 AM - 9:30 AM" },
+  { key: 4, name: "9:30 AM - 10:00 AM" },
+  { key: 5, name: "10:00 AM - 10:30 AM" },
+  { key: 6, name: "10:30 AM - 11:00 AM" },
+  { key: 7, name: "11:00 AM - 11:30 AM" },
+  { key: 8, name: "11:30 AM - 12:00 PM" },
+  { key: 9, name: "12:00 PM - 12:30 PM" },
+  { key: 10, name: "12:30 PM - 1:00 PM" },
+  { key: 11, name: "1:00 PM - 1:30 PM" },
+  { key: 12, name: "1:30 PM - 2:00 PM" },
+  { key: 13, name: "2:00 PM - 2:30 PM" },
+  { key: 14, name: "2:30 PM - 3:00 PM" },
+  { key: 15, name: "3:00 PM - 3:30 PM" },
+  { key: 16, name: "3:30 PM - 4:00 PM" },
+  { key: 17, name: "4:00 PM - 4:30 PM" },
+  { key: 18, name: "4:30 PM - 5:00 PM" },
+  { key: 19, name: "5:00 PM - 5:30 PM" },
+  { key: 20, name: "5:30 PM - 6:00 PM" },
+  { key: 21, name: "6:00 PM - 6:30 PM" },
+  { key: 22, name: "6:30 PM - 7:00 PM" },
+  { key: 23, name: "7:00 PM - 7:30 PM" },
+  { key: 24, name: "7:30 PM - 8:00 PM" },
+  { key: 25, name: "8:00 PM - 8:30 PM" },
+  { key: 26, name: "8:30 PM - 9:00 PM" },
+  { key: 27, name: "9:00 PM - 9:30 PM" },
+  { key: 28, name: "9:30 PM - 10:00 PM" },
+  { key: 29, name: "10:00 PM - 10:30 PM" },
+  { key: 30, name: "10:30 PM - 11:00 PM" },
+  { key: 31, name: "11:00 PM - 11:30 PM" },
+  { key: 32, name: "11:30 PM - 12:00 AM" },
 ];
 
-
-export default function Timeslots({ nextButtonDisabled, setNextButtonDisabled }: { nextButtonDisabled: boolean, setNextButtonDisabled: Function }) {
+export default function Timeslots({
+  nextButtonDisabled,
+  setNextButtonDisabled,
+}: {
+  nextButtonDisabled: boolean;
+  setNextButtonDisabled: Function;
+}) {
   const [selectedSlots, setSelectedSlots] = useState(new Set<number>([]));
-  const [dragOperation, setDragOperation] = useState<"Selecting" | "Deselecting" | "None">("None");
+  const [dragOperation, setDragOperation] = useState<
+    "Selecting" | "Deselecting" | "None"
+  >("None");
   const [dragStartIndex, setDragStartIndex] = useState<number>(-1);
 
   // some pretty ugly logic...
   // basically, clicking will start a drag where slots are selected/deselected
-  // + some other features to make it nice 
+  // + some other features to make it nice
   const handleMouseDown = (key: number) => {
     setDragStartIndex(key);
     // if we're clicking on a selected slot, deselect it
@@ -75,14 +82,18 @@ export default function Timeslots({ nextButtonDisabled, setNextButtonDisabled }:
       }
     }
     setSelectedSlots(new Set(selectedSlots));
-  }
+  };
 
   // logic to select/deselect slots while dragging
   const handleDrag = (key: number) => {
     // if we simply select/deselect the slot, then slots can get skipped over
     // because the mouse moves faster than the event handler can fire
     // so we need to make sure to select/deselect all the slots in between
-    for (let i = Math.min(dragStartIndex, key); i <= Math.max(dragStartIndex, key); i++) {
+    for (
+      let i = Math.min(dragStartIndex, key);
+      i <= Math.max(dragStartIndex, key);
+      i++
+    ) {
       if (dragOperation === "Selecting" && !selectedSlots.has(i)) {
         selectedSlots.add(i);
       }
@@ -91,21 +102,21 @@ export default function Timeslots({ nextButtonDisabled, setNextButtonDisabled }:
       }
     }
     setSelectedSlots(new Set(selectedSlots));
-  }
+  };
 
   // stop dragging when we let go of the mouse or it leaves the timeslots div
   const onMouseUp = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
     e.preventDefault();
     setDragOperation("None");
-  }
+  };
   const onMouseLeave = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
     e.preventDefault();
     setDragOperation("None");
-  }
+  };
   // if we don't include this, the browser will select text and everything will get messed up
   const onMouseDown = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
     e.preventDefault();
-  }
+  };
 
   useEffect(() => {
     setNextButtonDisabled(selectedSlots.size == 0);
@@ -114,35 +125,41 @@ export default function Timeslots({ nextButtonDisabled, setNextButtonDisabled }:
   return (
     <div className="flex flex-col">
       <div
-        className="flex flex-col border-4 border-red rounded-xl w-[300px] h-full"
+        className="flex flex-col border-2 border-red rounded-md w-[300px] h-full"
         id="timeslots"
         onMouseDown={onMouseDown}
         onMouseUp={onMouseUp}
         onMouseLeave={onMouseLeave}
       >
-        {timeslots.map((slot, i) =>
+        {timeslots.map((slot, i) => (
           <div
             key={slot.key}
-            className={`relative border-2 border-red flex-1 ${selectedSlots.has(slot.key) ? "bg-yellow" : ""}`}
+            className={`relative border-2 border-red flex-1 ${
+              selectedSlots.has(slot.key) ? "bg-yellow" : ""
+            }`}
             onMouseEnter={() => handleDrag(slot.key)}
             onMouseDown={() => handleMouseDown(slot.key)}
           >
-            {i % 2 == 0 && // show the time on every other slot
+            {i % 2 == 0 && ( // show the time on every other slot
               <div className="absolute text-red z-10 -mt-[14px] ml-[100%] pl-2">
                 {indexToTime(i)}
               </div>
-            }
+            )}
           </div>
-        )}
+        ))}
       </div>
-      {
-        selectedSlots.size == 0 ? "Select a time slot" :
-          `You selected ${indexToTime(Math.min(...Array.from(selectedSlots)) - 1)} to ${indexToTime(Math.max(...Array.from(selectedSlots)))}`
-      }.
+      {selectedSlots.size == 0
+        ? "Select a time slot"
+        : `You selected ${indexToTime(
+            Math.min(...Array.from(selectedSlots)) - 1
+          )} to ${indexToTime(Math.max(...Array.from(selectedSlots)))}`}
+      .
     </div>
-  )
+  );
 }
 
 function indexToTime(i: number) {
-  return `${Math.floor(i / 2 + 7) % 12 + 1}:${i % 2 == 0 ? "0" : "3"}0${i / 2 + 8 <= 11 ? "AM" : "PM"}`;
+  return `${(Math.floor(i / 2 + 7) % 12) + 1}:${i % 2 == 0 ? "0" : "3"}0${
+    i / 2 + 8 <= 11 ? "AM" : "PM"
+  }`;
 }
